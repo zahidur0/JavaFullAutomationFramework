08f04eb55d9f0e004d9c7b02c25b2a59b05a335e
zahidur0 <88269984+zahidur0@users.noreply.github.com>
2022-03-30 16:23:44 +0100

    Improve SQLite data driven testing class

--- src/test/java/com/UKTalentHubJava/test_cases/testng/TCT02_SqlLoginTest.java
+++ src/test/java/com/UKTalentHubJava/test_cases/testng/TCT02_SqlLoginTest.java
@@ -12,26 +12,41 @@ import java.util.Arrays;
 
 public class TCT02_SqlLoginTest extends BaseClassAutomationPractice {
 
-    @Test(dataProvider = "SqlLoginData")
-    public void LoginDDT(String uname, String pwd) throws IOException {
+    @Test(dataProvider = "IncorrectSqlLoginData")
+    public void IncorrectLoginDDT(String uname, String pwd) throws IOException {
         AutomationPractice ap = new AutomationPractice(driver);
         ap.setUsername(uname);
-        logger.info("Entered username");
+        logger.debug("Entered username");
         ap.setPassword(pwd);
-        logger.info("Entered password");
+        logger.debug("Entered password");
         ap.clickLogin();
 
         if (ap.incorrectPasswordMessageExists()) {
-            logger.info("Login failed");
-            new ScreenshotTaker(driver);
-            Assert.fail("Login failed");
+            logger.info("Incorrect login test successful");
         } else {
-            if (driver.getTitle().equals("My account - My Store")) {
-                logger.info("Login successful");
-            } else {
-                Assert.fail("Account page not reached");
-            }
+            new ScreenshotTaker(driver);
+            logger.error("Incorrect login test failed");
+            Assert.fail("Incorrect login test failed");
         }
+    }
+
+    @Test(dataProvider = "CorrectSqlLoginData")
+    public void CorrectLoginDDT(String uname, String pwd) throws IOException {
+        AutomationPractice ap = new AutomationPractice(driver);
+        ap.setUsername(uname);
+        logger.debug("Entered username");
+        ap.setPassword(pwd);
+        logger.debug("Entered password");
+        ap.clickLogin();
+
+        if (driver.getTitle().equals("My account - My Store")) {
+            logger.info("Login successful");
+        } else {
+            new ScreenshotTaker(driver);
+            logger.error("Login failed");
+            Assert.fail("Login failed");
+        }
+
     }
 
     private Connection connect() {
@@ -47,22 +62,50 @@ public class TCT02_SqlLoginTest extends BaseClassAutomationPractice {
         return conn;
     }
 
-    public int countRows() throws SQLException {
-        String sql = "SELECT COUNT(*) FROM correct_login_details";
+    public int countRows(String tableName) throws SQLException {
+        String sql = "SELECT COUNT(*) FROM " + tableName;
         Connection conn = this.connect();
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
-        return rs.getInt(1);
+        int rowNum = rs.getInt(1);
+        conn.close();
+        return rowNum;
     }
 
-    @DataProvider(name = "SqlLoginData")
-    public Object[][] selectAll() throws SQLException {
+    @DataProvider(name = "IncorrectSqlLoginData")
+    public Object[][] selectAllIncorrect() throws SQLException {
+        String sql = "SELECT username, password FROM incorrect_login_details";
+        Connection conn = this.connect();
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        int colNum = rs.getMetaData().getColumnCount();
+        int rowNum = countRows("incorrect_login_details");
+        System.out.println("incorrect row num: " + rowNum);
+        // data provider must return an Object[][] or Iterable<Object[]>
+        Object[][] loginDetails = new Object[rowNum][colNum];
+        System.out.println(rowNum);
+
+        for (int i = 0; i < rowNum; i++) {
+            rs.next();
+            for (int j = 0; j <= (colNum - 1); j++) {
+                System.out.println(i + " " + j);
+                System.out.println(rs.getString(j + 1));
+                loginDetails[i][j] = rs.getString(j + 1);
+            }
+        }
+        conn.close();
+        System.out.println(Arrays.deepToString(loginDetails));
+        return loginDetails;
+    }
+
+    @DataProvider(name = "CorrectSqlLoginData")
+    public Object[][] selectAllCorrect() throws SQLException {
         String sql = "SELECT username, password FROM correct_login_details";
         Connection conn = this.connect();
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
         int colNum = rs.getMetaData().getColumnCount();
-        int rowNum = countRows();
+        int rowNum = countRows("correct_login_details");
         // data provider must return an Object[][] or Iterable<Object[]>
         Object[][] loginDetails = new Object[rowNum][colNum];
         System.out.println(rowNum);
---
+++ src/test/java/com/UKTalentHubJava/test_data/LoginDetails.sql
@@ -0,0 +1,30 @@
+CREATE TABLE correct_login_details (
+    id Integer PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE incorrect_login_details (
+    id Integer PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(255) UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+DROP TABLE correct_login_details;
+DROP TABLE incorrect_login_details;
+
+INSERT INTO incorrect_login_details (username, password) VALUES
+("lindsey_marni@email.com", "l1nds3y"),
+("jude_heath@email.com", "jud3h34th"),
+("rebecca_josie@email.com", "r3b3cc4"),
+("morris_conway@email.com", "c0nw4y"),
+("matt_rolland@email.com", "m4ttr0114nd");
+
+INSERT INTO correct_login_details (username, password) VALUES
+("123123@email.com", "123123");
+
+SELECT COUNT(*) FROM correct_login_details;
+SELECT COUNT(*) FROM incorrect_login_details;
+SELECT id, username, password FROM correct_login_details;
+
+SELECT id, username, password FROM incorrect_login_details;
